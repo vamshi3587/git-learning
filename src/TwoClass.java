e6f820aca5200b79cf3282fad15ace0cbd575aab
vamshi3587 <61615694+vamshi3587@users.noreply.github.com>
2021-09-29 22:47:31 +0530

    added all files to local repository

---
+++ src/TwoClass.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoClass {
+public static void main(String[] args) {
+	List <Boolean> list = new ArrayList<>();
+
+list.add(true);
+
+list.add(Boolean.parseBoolean("False"));
+
+list.add(Boolean.TRUE);
+
+System.out.print(list.size()); System.out.print(list.get (1) instanceof Boolean);
+}
+}
