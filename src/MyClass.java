ec13090b02bea565f0c33c758aba7258a6266fc8
vamshi3587 <61615694+vamshi3587@users.noreply.github.com>
2021-09-29 23:16:16 +0530

    Added equals Ignore case

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,6 +2,9 @@ public class MyClass {
 
 	public static void printmethod(String s) {
 		System.out.println(s);
+		if(s.equalsIgnoreCase(s)) {
+			System.out.println(s);
+		}
 	}
 public static void main(String[] args) { 
 printmethod("Vamshi");
