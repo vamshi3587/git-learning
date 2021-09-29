4dcee5008cb2f8655c2b66853f52218deb1dab70
vamshi3587 <61615694+vamshi3587@users.noreply.github.com>
2021-09-29 22:55:19 +0530

    Second Commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+public class MyClass {
 
-class Main {
-
+	public static void printmethod(String s) {
+		System.out.println(s);
+	}
 public static void main(String[] args) { 
-
+printmethod("Vamshi");
 }
 }
