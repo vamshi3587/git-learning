4539edbb68b65d40169de861f5dcce3493d4f8c2
vamshi3587 <61615694+vamshi3587@users.noreply.github.com>
2021-09-29 23:28:32 +0530

    This method added after cloning
    This method added after cloning to local repository by other developers

--- src/MyClass.java
+++ src/MyClass.java
@@ -6,6 +6,9 @@ public class MyClass {
 			System.out.println(s);
 		}
 	}
+	public void methodAdded(){
+		System.out.println("This method added after cloning to local repository by other developers");
+	}
 public static void main(String[] args) { 
 printmethod("Vamshi");
 }
